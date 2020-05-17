502e87b124d810e54f53de33264738b0b8877b55
Tevinthuku <tevinthuku@gmail.com>
2020-05-17 15:10:41 +0300

    get rid of uneeded files
    - remove redux files & configurations
    - remove redux dependencies
    - remove unneeded css files

--- app/index.tsx
+++ app/index.tsx
@@ -1,26 +1,26 @@
 import React, { Fragment } from 'react';
 import { render } from 'react-dom';
 import { AppContainer as ReactHotAppContainer } from 'react-hot-loader';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { ThemeProvider } from '@material-ui/core/styles';
 import Root from './containers/Root';
-import { configureStore, history } from './store/configureStore';
-
 import theme from './constants/theme';
-
-const store = configureStore();
 
 const AppContainer = process.env.PLAIN_HMR ? Fragment : ReactHotAppContainer;
 
 document.addEventListener('DOMContentLoaded', () =>
   render(
-    <ThemeProvider theme={theme}>
-      {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-      <CssBaseline />
-      <AppContainer>
-        <Root store={store} history={history} />
-      </AppContainer>
-    </ThemeProvider>,
+    <Router>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <AppContainer>
+          <Root />
+        </AppContainer>
+      </ThemeProvider>
+    </Router>,
     document.getElementById('root')
   )
 );
